2ad69b40365e5a7646f0e8aa8c005582eec70c26
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2024-02-24 16:48:12 -0800

    add six piece 1-5 and four piece 6-4 to auto selection

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -10,11 +10,12 @@ public enum AutoSelection {
   ONE_PIECE("Red 1 Piece", ""),
   TWO_PIECE_2("Red 2 Piece 2", ""),
   THREE_PIECE_1_2("Red 3 Piece 1-2", ""),
-  FOUR_PIECE_1_3("Red 4 Piece 1-3", ""),
+  FOUR_PIECE_6_4("Red 4 Piece 6-4", ""),
   FIVE_PIECE_1_4("Red 5 Piece 1-4", ""),
   FIVE_PIECE_2_5("Red 5 Piece 2-5", ""),
   FIVE_PIECE_3_6("Red 5 Piece 3-6", ""),
-  SIX_PIECE_1_5("Red 6 Piece 1-5", "");
+  SIX_PIECE_1_5("Red 6 Piece 1-5", ""),
+  SIX_PIECE_1_5_TEST("Red 6 Piece 1-5 Test", "");
 
   public final String redAutoName;
   public final String blueAutoName;
