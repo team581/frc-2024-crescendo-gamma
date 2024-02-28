0ad387118aaebd3122c613fe13d3a8d1635ec6d5
Owen <117956892+Owen756@users.noreply.github.com>
2024-02-27 19:54:39 -0800

    Enable open loop driving

--- src/main/java/frc/robot/swerve/SwerveSubsystem.java
+++ src/main/java/frc/robot/swerve/SwerveSubsystem.java
@@ -224,7 +224,7 @@ public class SwerveSubsystem extends LifecycleSubsystem {
 
           Logger.recordOutput("Swerve/UsedTeleopSpeeds", teleopSpeeds);
 
-          setFieldRelativeSpeeds(teleopSpeeds, true);
+          setFieldRelativeSpeeds(teleopSpeeds, false);
         });
   }
 
