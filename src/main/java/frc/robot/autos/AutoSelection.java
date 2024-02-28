46fe5ed1abebffd5b4735eb0a8ce2e8d26b84151
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2024-02-27 21:05:55 -0800

    changes to auto selection

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -7,14 +7,13 @@ package frc.robot.autos;
 public enum AutoSelection {
   DO_NOTHING("", ""),
 
-  FOUR_PIECE_6_4("Red 4 Piece 6-4", "Blue 4 Piece 6-4"),
-  FIVE_PIECE_1_4("Red 5 Piece 1-4", "Blue 5 Piece 1-4"),
-  FIVE_PIECE_2_5("Red 5 Piece 2-5", "Blue 5 Piece 2-5"),
-  FIVE_PIECE_3_6("Red 5 Piece 3-6", "Blue 5 Piece 3-6"),
+  //FOUR_PIECE_6_4("Red 4 Piece 6-4", "Blue 4 Piece 6-4"),
+ // FIVE_PIECE_2_5("Red 5 Piece 2-5", "Blue 5 Piece 2-5"),
+  //FIVE_PIECE_3_6("Red 5 Piece 3-6", "Blue 5 Piece 3-6"),
   FIVE_PIECE_8_5("Red 5 Piece 8-5", "Blue 5 Piece 8-5"),
-  FIVE_PIECE_RVL_8_5("Red 5 Piece RVL 8-5", "Blue 5 Piece RVL 8-6"),
-  SIX_PIECE_1_5("Red 6 Piece 1-5", "Blue 6 Piece 1-5"),
-  SIX_PIECE_1_5_TEST("Red 6 Piece 1-5 Test", "Blue 6 Piece 1-5 Test");
+  //FIVE_PIECE_RVL_8_5("Red 5 Piece RVL 8-5", "Blue 5 Piece RVL 8-6"),
+  FIVE_PIECE_4_2("Red 5 Piece 4-5, 3-2", ""),
+  SIX_PIECE_1_5("Red 6 Piece 1-5, 6", "Blue 6 Piece 1-5 Test");
 
   public final String redAutoName;
   public final String blueAutoName;
