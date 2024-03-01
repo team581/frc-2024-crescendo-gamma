aa26fa599333d0fd40601609d9953280f77e62ba
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2024-03-01 14:28:32 -0800

    Lower elevator positions when climbing

--- src/main/java/frc/robot/elevator/ElevatorPositions.java
+++ src/main/java/frc/robot/elevator/ElevatorPositions.java
@@ -8,8 +8,8 @@ public class ElevatorPositions {
   public static final double STOWED = 0;
   public static final double AMP_OUTTAKE = 15;
 
-  public static final double CLIMBING = 22;
-  public static final double TRAP_SHOT = 22;
+  public static final double CLIMBING = 21;
+  public static final double TRAP_SHOT = 21;
   public static final double CLIMBING_FINISHED = 16;
 
   private ElevatorPositions() {}
