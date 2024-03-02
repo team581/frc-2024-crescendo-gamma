9694c80370c2707f8c94e840120643bf97f37054
Fernanda <105395555+fcuellar13@users.noreply.github.com>
2024-03-01 23:40:16 -0800

    Fernanda-autos (#84)
    * comment out 5 piece amp 4-2 auto and update names of autos to be accurate
    
    * add red and blue paths to their corresponding folder -4 Piece Source 8-6
    
    * add all red and blue paths to correct folders -6 Piece Source 1-5 auto
    
    * adjust shooting position for blue LWS (for Blue 4 Piece Source 8-6)
    
    * adjust blue RWS for 6 Piece Source 1-5
    
    * adjust red lws for Red 4 Piece Source 8-6 auto
    
    * adjust heading lws to 7 to avoid hitting truss
    
    * format

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -10,10 +10,10 @@ public enum AutoSelection {
   // FOUR_PIECE_6_4("Red 4 Piece 6-4", "Blue 4 Piece 6-4"),
   // FIVE_PIECE_2_5("Red 5 Piece 2-5", "Blue 5 Piece 2-5"),
   // FIVE_PIECE_3_6("Red 5 Piece 3-6", "Blue 5 Piece 3-6"),
-  FIVE_PIECE_SOURCE_8_5("Red 5 Piece 8-5", "Blue 5 Piece 8-5"),
+  FOUR_PIECE_SOURCE_8_6("Red 4 Piece Source 8-6", "Blue 4 Piece Source 8-6"),
   // FIVE_PIECE_RVL_8_5("Red 5 Piece RVL 8-5", "Blue 5 Piece RVL 8-6"),
-  FIVE_PIECE_AMP_4_2("Red 5 Piece 4-2", "Blue 5 Piece 4-2"),
-  SIX_PIECE_SOURCE_1_5("Red 6 Piece 1-5", "Blue 6 Piece 1-5");
+  // FIVE_PIECE_AMP_4_2("Red 5 Piece 4-2", "Blue 5 Piece 4-2"),
+  SIX_PIECE_SOURCE_1_5("Red 6 Piece Source 1-5", "Blue 6 Piece Source 1-5");
 
   public final String redAutoName;
   public final String blueAutoName;
