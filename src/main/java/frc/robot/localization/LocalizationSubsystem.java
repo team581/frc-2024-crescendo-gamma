a28dae1ff5dff06bee7f1523954646cbe24fca69
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2024-03-01 22:21:48 -0800

    format

--- src/main/java/frc/robot/localization/LocalizationSubsystem.java
+++ src/main/java/frc/robot/localization/LocalizationSubsystem.java
@@ -38,7 +38,8 @@ public class LocalizationSubsystem extends LifecycleSubsystem {
   private final VisionSubsystem vision;
   private double lastAddedVisionTimestamp = 0;
 
-  private final TimedDataBuffer xHistory = new TimedDataBuffer(RobotConfig.get().vision().translationHistoryArraySize());
+  private final TimedDataBuffer xHistory =
+      new TimedDataBuffer(RobotConfig.get().vision().translationHistoryArraySize());
   private final TimedDataBuffer yHistory =
       new TimedDataBuffer(RobotConfig.get().vision().translationHistoryArraySize());
 
