d1652a1e9c66a05c6aaffd1ad3937306219b614a
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2024-03-14 16:52:40 -0700

    format

--- src/main/java/frc/robot/robot_manager/RobotState.java
+++ src/main/java/frc/robot/robot_manager/RobotState.java
@@ -4,7 +4,6 @@
 
 package frc.robot.robot_manager;
 
-import edu.wpi.first.hal.FRCNetComm.tResourceType;
 import edu.wpi.first.wpilibj.util.Color;
 import frc.robot.lights.BlinkPattern;
 import frc.robot.lights.LightsState;
@@ -13,15 +12,13 @@ public enum RobotState {
   /** Idling without a note. */
   IDLE_NO_GP(false, false, false, new LightsState(Color.kBlue, BlinkPattern.SOLID)),
   /** Idling with a note in the conveyor, going to stow everything. */
-  PREPARE_IDLE_WITH_GP_FROM_CONVEYOR(
-      true, false, false, new LightsState(null, BlinkPattern.SOLID)),
+  PREPARE_IDLE_WITH_GP_FROM_CONVEYOR(true, false, false, new LightsState(null, BlinkPattern.SOLID)),
   /** Idling with a note in the queuer. */
   IDLE_WITH_GP(true, false, false, new LightsState(null, BlinkPattern.SOLID)),
 
   /** Intaking a game piece. Transition to INTAKE_TO_QUEUER when done. */
   INTAKING(false, false, false, new LightsState(Color.kBlue, BlinkPattern.BLINK_SLOW)),
   FINISH_INTAKING(true, false, false, new LightsState(Color.kBlue, BlinkPattern.BLINK_SLOW)),
-
 
   /** Outtaking via the shooter. Game piece should be in queuer at start. */
   OUTTAKING_SHOOTER(true, false, false, new LightsState(Color.kGreen, BlinkPattern.BLINK_FAST)),
