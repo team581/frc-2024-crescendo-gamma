5d5856008ab7b4c2961cb98078840446b0528cc4
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2024-03-30 14:43:31 -0700

    big green square

--- src/main/java/frc/robot/robot_manager/RobotManager.java
+++ src/main/java/frc/robot/robot_manager/RobotManager.java
@@ -6,6 +6,7 @@ package frc.robot.robot_manager;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.climber.ClimberMode;
@@ -661,6 +662,8 @@ public class RobotManager extends LifecycleSubsystem {
 
     // Reset all flags
     flags.clear();
+
+    SmartDashboard.putBoolean("HasNote", getState().hasNote);
   }
 
   public void waitPodiumShotRequest() {
