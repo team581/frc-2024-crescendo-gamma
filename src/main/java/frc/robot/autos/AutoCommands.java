a9bc4cbf46e20a65f242549ed3924659eb9a8917
Jonah Snider <jonah@jonahsnider.com>
2024-03-30 00:07:30 -0700

    Add AutoCommands.followPathForAlliance()

--- src/main/java/frc/robot/autos/AutoCommands.java
+++ src/main/java/frc/robot/autos/AutoCommands.java
@@ -8,12 +8,20 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.fms.FmsSubsystem;
 import frc.robot.robot_manager.RobotCommands;
 import frc.robot.robot_manager.RobotManager;
 import frc.robot.robot_manager.RobotState;
 import java.util.function.BooleanSupplier;
 
 public class AutoCommands {
+  private static Command followPathForAlliance(PathPlannerPath redPath, PathPlannerPath bluePath) {
+    return Commands.either(
+        AutoBuilder.followPath(redPath),
+        AutoBuilder.followPath(bluePath),
+        FmsSubsystem::isRedAlliance);
+  }
+
   private final RobotCommands actions;
   private final RobotManager robotManager;
 
