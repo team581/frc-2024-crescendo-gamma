c33fc8fbc021707912ce726d941f9cd5e2fe8512
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2024-04-02 20:12:04 -0700

    Turn off calibration mode

--- src/main/java/frc/robot/vision/VisionSubsystem.java
+++ src/main/java/frc/robot/vision/VisionSubsystem.java
@@ -24,7 +24,7 @@ import java.util.Optional;
 import org.littletonrobotics.junction.Logger;
 
 public class VisionSubsystem extends LifecycleSubsystem {
-  private static final boolean CALIBRATION_RIG_ENABLED = true;
+  private static final boolean CALIBRATION_RIG_ENABLED = false;
   private static final boolean SHOOT_TO_SIDE_ENABLED = true;
 
   public static final Pose2d ORIGINAL_RED_SPEAKER =
@@ -298,7 +298,11 @@ public class VisionSubsystem extends LifecycleSubsystem {
       return VisionState.OFFLINE;
     }
 
-    if (LimelightHelpers.getTV("")) {
+    if (RobotConfig.get().vision().strategy() == VisionStrategy.TX_TY_AND_MEGATAG) {
+      return getDistanceAngleTxTy().isPresent() ? VisionState.SEES_TAGS : VisionState.ONLINE_NO_TAGS;
+    }
+
+    if (getVisionResult().isPresent()) {
       return VisionState.SEES_TAGS;
     }
 
