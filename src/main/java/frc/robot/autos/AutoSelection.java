661edf1b556033dc0fcbc60784a3d84fdbadf175
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2024-04-04 16:21:08 -0700

    add dynamic to auto naming

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -7,10 +7,10 @@ package frc.robot.autos;
 public enum AutoSelection {
   DO_NOTHING("", ""),
 
-  FOUR_PIECE_SOURCE_8_6("Red 4 Piece Source 8-6", "Blue 4 Piece Source 8-6"),
-  FIVE_PIECE_3_6_DEV("Red Right 5 Piece", "Blue Right 5 Piece"),
-  SIX_PIECE_2_5_DEV("Red 6 Piece 2,1,3-5", "Blue 6 Piece 2,1,3-5"),
-  SIX_PIECE_2_6_DEV("Red 6 Piece 2,1,3,5,6", "Blue 6 Piece 2,1,3,5,6");
+  FOUR_PIECE_SOURCE_8_6_DYN("Red 4 Piece Source 8-6", "Blue 4 Piece Source 8-6"),
+  FIVE_PIECE_3_6_DYN("Red Right 5 Piece", "Blue Right 5 Piece"),
+  SIX_PIECE_2_5_DYN("Red 6 Piece 2,1,3-5", "Blue 6 Piece 2,1,3-5"),
+  SIX_PIECE_2_6_DYN("Red 6 Piece 2,1,3,5,6", "Blue 6 Piece 2,1,3,5,6");
 
   public final String redAutoName;
   public final String blueAutoName;
