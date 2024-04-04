9d64eacd2a8315b8ce6bf457664885674b49b595
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2024-04-04 16:43:50 -0700

    rename five piece auto

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -8,7 +8,7 @@ public enum AutoSelection {
   DO_NOTHING("", ""),
 
   FOUR_PIECE_SOURCE_8_6_DYN("Red 4 Piece Source 8-6", "Blue 4 Piece Source 8-6"),
-  FIVE_PIECE_3_6_DYN("Red Right 5 Piece", "Blue Right 5 Piece"),
+  OP_FIVE_PIECE_DYN("Red Right 5 Piece", "Blue Right 5 Piece"),
   SIX_PIECE_2_5_DYN("Red 6 Piece 2,1,3-5", "Blue 6 Piece 2,1,3-5"),
   SIX_PIECE_2_6_DYN("Red 6 Piece 2,1,3,5,6", "Blue 6 Piece 2,1,3,5,6");
 
