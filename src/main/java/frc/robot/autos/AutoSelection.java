61c47e69774f77873348dce8fd0f32e3f4a69820
Owen <117956892+Owen756@users.noreply.github.com>
2024-04-06 08:10:16 -0700

    Add new auto to auto selection

--- src/main/java/frc/robot/autos/AutoSelection.java
+++ src/main/java/frc/robot/autos/AutoSelection.java
@@ -8,6 +8,7 @@ public enum AutoSelection {
   DO_NOTHING("", ""),
 
   FOUR_PIECE_SOURCE_8_6_DYN("Red 4 Piece Source 8-6", "Blue 4 Piece Source 8-6"),
+  FOUR_PIECE_SOURCE_7_8_DYN("Red 4 Piece Source 7,6,8", "Blue 4 Piece Source 7,6,8"),
   OP_FIVE_PIECE_DYN("Red Right 5 Piece", "Blue Right 5 Piece"),
   SIX_PIECE_2_5_DYN("Red 6 Piece 2,1,3-5", "Blue 6 Piece 2,1,3-5"),
   SIX_PIECE_2_6_DYN("Red 6 Piece 2,1,3,5,6", "Blue 6 Piece 2,1,3,5,6");
