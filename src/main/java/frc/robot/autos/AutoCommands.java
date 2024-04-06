0686a1757a8ec5e31603d8d14128224880398dae
Owen <117956892+Owen756@users.noreply.github.com>
2024-04-06 15:28:32 -0700

    Remove 6th note from midline alt command

--- src/main/java/frc/robot/autos/AutoCommands.java
+++ src/main/java/frc/robot/autos/AutoCommands.java
@@ -126,16 +126,12 @@ public class AutoCommands {
     var red5ToCenterWingShot = PathPlannerPath.fromPathFile("Red 5 to CWS");
     var redCenterWingShotTo6 = PathPlannerPath.fromPathFile("Red CWS to 6");
     var red6ToStageWingShot = PathPlannerPath.fromPathFile("Red 6 to SWS");
-    var redStageWingShotTo4 = PathPlannerPath.fromPathFile("Red SWS to 4");
-    var red4ToRightWingShot = PathPlannerPath.fromPathFile("Red 4 to RWS");
 
     var blue5To6 = PathPlannerPath.fromPathFile("Blue 5 to 6");
     var blue6To4 = PathPlannerPath.fromPathFile("Blue 6 to 4");
     var blue5ToCenterWingShot = PathPlannerPath.fromPathFile("Blue 5 to CWS");
     var blueCenterWingShotTo6 = PathPlannerPath.fromPathFile("Blue CWS to 6");
     var blue6ToStageWingShot = PathPlannerPath.fromPathFile("Blue 6 to SWS");
-    var blueStageWingShotTo4 = PathPlannerPath.fromPathFile("Blue SWS to 4");
-    var blue4ToRightWingShot = PathPlannerPath.fromPathFile("Blue 4 to RWS");
 
     return Commands.sequence(
         Commands.either(
@@ -146,13 +142,10 @@ public class AutoCommands {
             this::hasNote),
         Commands.either(
                 followPathForAlliance(red6ToStageWingShot, blue6ToStageWingShot)
-                    .andThen(speakerShotWithTimeout())
-                    .andThen(followPathForAlliance(redStageWingShotTo4, blueStageWingShotTo4)),
+                    .andThen(speakerShotWithTimeout()),
                 followPathForAlliance(red6To4, blue6To4),
                 this::hasNote)
-            .andThen(
-                followPathForAlliance(red4ToRightWingShot, blue4ToRightWingShot)
-                    .andThen(speakerShotWithTimeout())));
+);
   }
 
   public Command getMidlineNotesSourceCommand() {
