e2b7ee960c10b674816aeadedbe701e0e5a106db
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2024-04-20 10:45:40 -0500

    Reduce wrist position commanded for subwoofer shot

--- src/main/java/frc/robot/wrist/WristPositions.java
+++ src/main/java/frc/robot/wrist/WristPositions.java
@@ -15,7 +15,7 @@ public class WristPositions {
   public static final Rotation2d SHOOTER_AMP = Rotation2d.fromDegrees(58.0);
 
   public static final Rotation2d SUBWOOFER_SHOT = Rotation2d.fromDegrees(59.0);
-  public static final Rotation2d AUTO_SUBWOOFER_SHOT = Rotation2d.fromDegrees(61.0);
+  public static final Rotation2d AUTO_SUBWOOFER_SHOT = Rotation2d.fromDegrees(60.0);
   public static final Rotation2d PRESET_AMP = Rotation2d.fromDegrees(43);
   public static final Rotation2d PRESET_SOURCE = Rotation2d.fromDegrees(58.1);
   public static final Rotation2d PRESET_MIDDLE = Rotation2d.fromDegrees(58.1);
